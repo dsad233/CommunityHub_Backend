99aeafee22f457d49fc77b8b65260d58a8eed25f
dsad233 <ksl1421@naver.com>
2026-09-24 01:29:28 +0900

    fix: cron string -> enum 값으로 대체

--- src/cron/cron.controller.ts
+++ src/cron/cron.controller.ts
@@ -1,10 +1,13 @@
 import cron from 'node-cron';
 import { redis } from '../redis/redis.config';
+import { PrefixType } from '../common/libs/type';
 
 export function CronController() {
   // 달 1일 마다 초기화 처리
   cron.schedule('* 5 0 1 * *', async () => {
     // 인기 작성자 TOP3 목록 초기화
-    await redis.del('COUNT:POPULAR:POSTS');
+    await redis.del(
+      `${PrefixType.COUNT}:${PrefixType.POPULAR}:${PrefixType.POSTS}`,
+    );
   });
 }
